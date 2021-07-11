cbab650a8648f1c913e1a2f2877e7d7c8b882d8e
paul <paul.renobato@gmail.com>
2021-07-11 12:52:01 -0500

    added the html cards for each role of the team members and added the js input question asking for the role of the team members.

--- index.js
+++ index.js
@@ -5,4 +5,50 @@ const fs = require('fs');
 const inquirer = require('inquirer')
 const Manager1 = [];
 const Intern1 = [];
-const Engineer = [];
+const Engineer1 = [];
+
+const addTeamMember= () => {
+    inquirer
+        .prompt([
+            {
+            type: 'confirm',
+            name: 'addTeamMember',
+            message: 'Would you like to add another team member?',
+            },
+        ])
+        .then ((answers) => {
+            if (answers.addTeamMember === true) {
+            firstQuestion();
+            }
+            else {
+            console.log(manager1, engineer1, intern1);
+            module.exports= manager1;
+            module.exports= engineers1;
+            module.exports= intern1;
+            return answers;
+            }
+        });
+}
+const firstQuestion= () => {
+    inquirer
+        .prompt ([
+            {
+            type:'list',
+            name: 'role',
+            message: 'What is your role?',
+            choices: ['Manager', 'Engineer', 'Intern'],
+            },
+        ])
+        .then ((answers) => {
+            if (answers.role === 'Manager') {
+            managerQuestions();
+            }
+            else if (answers.role === 'Engineer') {
+            engineerQuestions();
+            }
+            else if (answers.role === 'Intern') {
+            internQuestions();
+            }
+        });
+};
+firstQuestion();
