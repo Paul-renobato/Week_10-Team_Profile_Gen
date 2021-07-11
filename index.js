93c17f57c2726bbe628c3aa8ebcc098295b41ba7
paul <paul.renobato@gmail.com>
2021-07-11 13:27:12 -0500

    Added the inputs for each role asking the questions when you want to add the team member.

--- index.js
+++ index.js
@@ -51,4 +51,113 @@ const firstQuestion= () => {
             }
         });
 };
+
 firstQuestion();
+
+const engineerQuestions = () => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is your name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is your ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is your email?",
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is your Github username?",
+        },
+      ])
+      .then((answers) => {
+        const newEngineer = new Engineer(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.github
+        );
+        Engineer1.push(newEngineer);
+        addTeamMember();
+      })
+  };
+
+  const internQuestions= () => {
+    inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your ID number?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Email?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'What school do you attend?',
+            },
+        ])
+        .then ((answers) => {
+            const newIntern= new Intern(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.school,
+            );
+            Intern1.push(newIntern);
+            addTeamMember();
+        });
+}
+
+const managerQuestions= () => {
+    inquirer
+        .prompt ([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your ID number?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your Email?',
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: 'What is your office number?',
+            },
+        ])
+        .then ((answers) => {
+            const newManager= new Manager(
+                answers.name,
+                answers.id,
+                answers.email,
+                answers.office,
+            );
+            Manager1.push(newManager);
+            addTeamMember();
+        })
+};
